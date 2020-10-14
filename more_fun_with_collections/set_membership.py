b31598a767a11d55a5d181c4ffc5a67fe6d6887b
djbittner <djbittner@dmacc.edu>
2020-10-13 19:23:48 -0500

    final

--- more_fun_with_collections/set_membership.py
+++ more_fun_with_collections/set_membership.py
@@ -4,6 +4,11 @@ Program: set_membership.py
 """
 
 
-def in_set():
-    pass
+def in_set(some_set, some_value):
+    return some_value in some_set
 
+
+if __name__ == '__main__':
+    my_set = set('12345')
+    print(my_set)
+    print('Is {}in {}?  {}'.format(5, my_set, in_set(my_set, str(5))))
