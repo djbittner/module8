1dd2dca971a6f4070d4738688505cf133acbedc0
djbittner <djbittner@dmacc.edu>
2020-10-19 20:13:55 -0500

    final commit dict_membership.py

--- more_fun_with_collections/dict_membership.py
+++ more_fun_with_collections/dict_membership.py
@@ -6,9 +6,11 @@ Program:  dict_membership.py
 
 
 def in_dict(some_dict, some_value):
-    pass
+    return some_value in some_dict
 
 
 if __name__ == '__main__':
-    pass
+    my_dict = dict({'A': 3, 'B': 4, 'C': 5, 'D': 6, 'E': 7})
+    print(my_dict)
+    print('Is {} in {}?  {}'.format(5, my_dict, in_dict(my_dict, str(5))))
 
